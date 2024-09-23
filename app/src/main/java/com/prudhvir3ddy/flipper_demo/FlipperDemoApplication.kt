f7f13a572603ec0b9be83f9da3c17e39b283ae34
prudhvir3ddy <prudhvi.reddy@deliveroo.com>
2024-09-23 15:54:44 +0530

    App init

---
+++ app/src/main/java/com/prudhvir3ddy/flipper_demo/FlipperDemoApplication.kt
@@ -0,0 +1,9 @@
+package com.prudhvir3ddy.flipper_demo
+
+import android.app.Application
+
+class FlipperDemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
